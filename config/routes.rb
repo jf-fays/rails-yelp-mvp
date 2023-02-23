f8cd4271332a47db27c3bf0ea68d4d25f7b648aa
jean-francois fays <fays.jf@gmail.com>
2023-02-23 08:06:32 +0100

    rails yelp mvp in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+r = Restaurant.new(name: 'Jean', address: '10 rue Perrin', phone_number:'0611111111', category: 'french')
+r.save
+r2 = Restaurant.new(name: 'Tah oh', address: '20 rue Perrin', phone_number:'0622222222', category: 'chinese')
+r2.save
+r3 = Restaurant.new(name: 'Mario', address: '30 rue Perrin', phone_number:'0633333333', category: 'italian')
+r3.save
+r4 = Restaurant.new(name: 'Chong', address: '40 rue Perrin', phone_number:'0644444444', category: 'japanese')
+r4.save
+r5 = Restaurant.new(name: 'Frite', address: '40 rue Perrin', phone_number:'0655555555', category: 'belgian')
+r5.save
